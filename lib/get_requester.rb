35cdb2cf3acb0e4124e3124c992ded17cf1d7523
Christie Yoon <cleex1999@gmail.com>
2020-08-17 20:49:38 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,8 +7,9 @@ class GetRequester
 
    URL = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
 
-   def initialize(URL)
-      @URL = URL
+   def initialize(url)
+      @url = url
+      @url = URL
    end
 
    def get_response_body
